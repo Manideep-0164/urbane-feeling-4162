9d3933786f3e4e02c0c03c8dbf5fbcbe281ecb32
Manideep-0164 <peddaboinimanideep03@gmail.com>
2023-02-26 22:21:13 +0530

    login, sign-up done

--- frontend/scripts/nav.js
+++ frontend/scripts/nav.js
@@ -1,5 +1,6 @@
 let navbar = document.querySelector("nav");
 let sticky = navbar.offsetTop;
+let userName = localStorage.getItem("name") || null;
 
 window.onscroll = function () {
     myFunction();
@@ -14,8 +15,10 @@ function myFunction() {
 }
 
 document.getElementById("user-name").addEventListener("click", () => {
-    document.querySelector(".login-div").classList.add("show-login");
-    document.querySelector(".overlay").classList.add("show-overlay");
+    if (userName == null) {
+        document.querySelector(".login-div").classList.add("show-login");
+        document.querySelector(".overlay").classList.add("show-overlay");
+    }
 })
 
 document.getElementById("exit").addEventListener("click", () => {
@@ -31,16 +34,37 @@ document.querySelector(".overlay").addEventListener("click", () => {
     document.querySelector(".overlay").classList.remove("show-overlay");
 })
 
-let userName = localStorage.getItem("name") || null;
-document.getElementById("user-name").textContent = userName || 'Login';
+document.getElementById("user-name").textContent = userName || 'Sign-in';
 
 if (userName) {
     document.getElementById("log-out").textContent = 'Logout';
     document.getElementById("log-out").addEventListener("click", () => {
         localStorage.removeItem('token');
         localStorage.removeItem('name');
+
+        // Opens a window with message Successfully Logged Out
+
+        // var w = window.open('', '', 'width=100,height=100')
+        // w.document.write('Successfully Logged Out')
+        // w.focus()
+        // setTimeout(function () { w.close(); }, 5000)
+
+        // Creates a small window 
+
+        // tempAlert("Successfully Logged Out", 5000);
+        alert("Successfully Logged Out")
         document.getElementById("log-out").textContent = 'About';
-        window.location.reload()
+        location.reload();
     })
 }
 else document.getElementById("log-out").textContent = 'About';
+
+function tempAlert(msg, duration) {
+    var el = document.createElement("div");
+    el.setAttribute("style", "position:absolute;top:40%;left:20%;background-color:white; width:30%; margin-auto;");
+    el.innerHTML = msg;
+    setTimeout(function () {
+        el.parentNode.removeChild(el);
+    }, duration);
+    document.body.appendChild(el);
+}
